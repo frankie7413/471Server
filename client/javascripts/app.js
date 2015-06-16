00fd321eef58a40e9e1d4b17677596f42cc4f4db
Francisco Rivas <frankie7413@gmail.com>
2015-06-16 00:03:59 -0700

    added more functionality for future implementation

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,12 +1,21 @@
 var main = function () {
 	'user strict'
+	var $result;
 
 	$("#filter").click(function() {
 		if($("#filtertext").val() === "Type here" || $("#filtertext").val() === ""){
 			alert("Enter a valid input");
 			$("#filtertext").val('');
 		}
+
+		userText($("#filtertext").val())
+
 	});
 
+	function userText(textoutput){
+		$("main .result").empty();
+		$result = $("<p>").text("Your input: " + textoutput);
+		$("main .result").append($result);
+	}
 }
 $(document).ready(main);
