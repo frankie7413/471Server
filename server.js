991a62b2ddc17041f3944176ff0aaef604cc6688
Francisco Rivas <frankie7413@gmail.com>
2015-06-16 02:03:54 -0700

    minor edit again

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ http.createServer(app).listen(8080, '45.55.220.106');
 
 app.use(express.static(__dirname + "/client"));
 
+app.get('/About', function (req, res) {
+	res.redirect('/about.html');
+})
+
 app.get('/404', function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('404 Error oh no!\n');
 })
 
-app.get('/About', function (req, res) {
-	res.redirect("/about.html");
-})
+
 
 console.log('Sever is running at frankie7413.me');
